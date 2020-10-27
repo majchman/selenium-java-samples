bd166540d2f1e9e9730e40071c8cd0d79ba44c56
MAjchman <majchman@users.noreply.github.com>
2020-10-27 18:49:23 +0100

    add a keyPresses Page Object with some tests

---
+++ src/main/java/majchman/testauto/ui/web/pageobjects/simple/KeyPressesPage.java
@@ -0,0 +1,45 @@
+package majchman.testauto.ui.web.pageobjects.simple;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class KeyPressesPage {
+
+    private static final String PAGE_URL = "https://the-internet.herokuapp.com/key_presses";
+    private static final String PAGE_HEADING = "Key Presses";
+
+    private static final By.ByCssSelector HEADING = new By.ByCssSelector( "h3" );
+    private static final By.ById INPUT_FIELD = new By.ById( "target" );
+    private static final By.ById RESULT_TEXT = new By.ById( "result" );
+
+    private final WebDriver driver;
+
+    public KeyPressesPage( WebDriver driver ) {
+        this.driver = driver;
+    }
+
+    public void open() {
+        driver.get( PAGE_URL );
+    }
+
+    public boolean isOpen() {
+        try {
+            return driver.getCurrentUrl().equals( PAGE_URL )
+                    && driver.findElement( HEADING ).getText().equals( PAGE_HEADING );
+        } catch ( NoSuchElementException e ) {
+            return false;
+        }
+    }
+
+    public void enterKeyPress( CharSequence keyValue ) {
+        WebElement inputField = driver.findElement( INPUT_FIELD );
+        inputField.click();
+        inputField.sendKeys( keyValue );
+    }
+
+    public String getKeyPressResult() {
+        return driver.findElement( RESULT_TEXT ).getText();
+    }
+}
---
+++ src/test/java/majchman/testauto/ui/web/pageobjects/simple/KeyPressesPageTest.java
@@ -0,0 +1,71 @@
+package majchman.testauto.ui.web.pageobjects.simple;
+
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class KeyPressesPageTest {
+
+    private static WebDriver driver;
+
+    private KeyPressesPage keyPressesPage;
+
+    @BeforeClass
+    public static void setUpDriver() {
+        System.setProperty( "webdriver.gecko.driver", "bin/firefoxdriver_win64.exe" );
+        driver = new FirefoxDriver();
+    }
+
+    @Before
+    public void createKeyPressesPage() {
+        this.keyPressesPage = new KeyPressesPage( driver );
+    }
+
+    @Test
+    public void keyPressesPageOpensCorrectly() {
+        keyPressesPage.open();
+
+        assertThat( "Key Presses Page should open correctly",
+                keyPressesPage.isOpen(),
+                is( true ) );
+    }
+
+    @Test
+    public void keyPressedIsShown() {
+        final String keyPressed = "t";
+
+        keyPressesPage.open();
+        keyPressesPage.enterKeyPress( "t" );
+
+        assertThat( String.format( "Pressed key '%s' should be shown", keyPressed ),
+                keyPressesPage.getKeyPressResult(),
+                is( equalTo( "You entered: " + keyPressed.toUpperCase() ) ) );
+    }
+
+    @Test
+    public void whenPressingKeySequence_onlyLastKeyIsShown() {
+        String[] keySequence = {"t", "e", "s"};
+
+        keyPressesPage.open();
+        String lastKeyPressed = null;
+        for ( String key : keySequence ) {
+            keyPressesPage.enterKeyPress( key );
+            lastKeyPressed = key;
+        }
+
+        assertThat( String.format( "Pressed key '%s' should be shown", lastKeyPressed ),
+                keyPressesPage.getKeyPressResult(),
+                is( equalTo( "You entered: " + lastKeyPressed.toUpperCase() ) ) );
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        if ( driver != null ) {
+            driver.quit();
+        }
+    }
+}
